3aa82d74541cb84129ed6f4dc29df212920a4619
95decode <95decode@gmail.com>
2022-12-22 15:28:13 +0900

    Init react

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import {
+  EthereumClient,
+  modalConnectors,
+  walletConnectProvider,
+} from "@web3modal/ethereum";
+
+import { Web3Modal } from "@web3modal/react";
+
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import './App.css';
+import Home from "./Home";
+
+const chains = [arbitrum, mainnet, polygon];
+
+// Wagmi client
+const { provider } = configureChains(chains, [
+  walletConnectProvider({ projectId: "<YOUR_PROJECT_ID>" }),
+]);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  provider,
+});
+
+// Web3Modal Ethereum Client
+const ethereumClient = new EthereumClient(wagmiClient, chains);
+
+function App() {
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Home />
+      </WagmiConfig>
+
+      <Web3Modal
+        projectId="<YOUR_PROJECT_ID>"
+        ethereumClient={ethereumClient}
+      />
+    </>
+  );
+}
+
+export default App;
