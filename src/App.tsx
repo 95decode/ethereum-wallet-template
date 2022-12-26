841b77360f3675d6934d064a64f14455f0352200
95decode <95decode@gmail.com>
2022-12-26 12:28:19 +0900

    Delete web3modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,19 @@
-import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
-} from "@web3modal/ethereum";
-
-import { Web3Modal } from "@web3modal/react";
-
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-
-import { arbitrum, mainnet, polygon } from "wagmi/chains";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "pages/Main";
+import NotFound from "pages/NotFound";
 import './App.css';
-import Home from "./Home";
-
-const chains = [arbitrum, mainnet, polygon];
-
-// Wagmi client
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: "<YOUR_PROJECT_ID>" }),
-]);
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: modalConnectors({ appName: "web3Modal", chains }),
-  provider,
-});
-
-// Web3Modal Ethereum Client
-const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 function App() {
-  return (
-    <>
-      <WagmiConfig client={wagmiClient}>
-        <Home />
-      </WagmiConfig>
-
-      <Web3Modal
-        projectId="<YOUR_PROJECT_ID>"
-        ethereumClient={ethereumClient}
-      />
-    </>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
