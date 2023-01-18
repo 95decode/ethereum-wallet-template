c50e3c3a2a707525f3d4b2454ecb923b8559b98c
95decode <95decode@gmail.com>
2023-01-18 17:40:14 +0900

    Delete sample logic

--- src/utils/EIP155RequestHandlerUtil.ts
+++ src/utils/EIP155RequestHandlerUtil.ts
@@ -9,7 +9,7 @@ import { formatJsonRpcError, formatJsonRpcResult } from '@json-rpc-tools/utils'
 import { SignClientTypes } from '@walletconnect/types'
 import { getSdkError } from '@walletconnect/utils'
 import { ethers, providers } from 'ethers'
-import axios from 'axios';
+//import axios from 'axios';
 
 export async function approveEIP155Request(
   requestEvent: SignClientTypes.EventArguments['session_request']
@@ -47,73 +47,9 @@ export async function approveEIP155Request(
       return formatJsonRpcResult(id, hash)
 
     case EIP155_SIGNING_METHODS.ETH_SIGN_TRANSACTION:
-      // for test
-      const unsignedTx = {
-        type: 0,
-        to: request.params[0].to,
-        value: request.params[0].value,
-        gasPrice: request.params[0].gasPrice,
-        gasLimit: request.params[0].gasLimit,
-        nonce: request.params[0].nonce,
-        chainId: request.params[0].chainId,
-        data: request.params[0].data
-      }
-/*
-      const unsignedTx = {
-        type: 2,
-        to: request.params[0].to,
-        value: request.params[0].value,
-        maxPriorityFeePerGas: ethers.utils.hexlify(200000000000),
-        maxFeePerGas: ethers.utils.hexlify(250000000000),
-        gasLimit: ethers.utils.hexlify(400000),
-        nonce: request.params[0].nonce,
-        chainId: 80001,
-        data: request.params[0].data
-      }
-*/
-      const serializedTx = ethers.utils.serializeTransaction(unsignedTx).slice(2);
-
-      const octet = process.env.OCTET_API_KEY;
-
-      const options = {
-        method: 'POST',
-        url: 'https://octet-api.blockchainapi.io/2.0/wallets/740/transactions/sign',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer ' + octet,
-          'content-type': 'application/json'
-        },
-        data: {
-          serializedUnsignedTransaction: serializedTx, 
-          address: request.params[0].from
-        }
-      };
-
-      const response = await axios.request(options)
-      console.log("res1 : ", response)
-
-      const wait = (timeToDelay: number) => new Promise((resolve) => setTimeout(resolve, timeToDelay))
-
-      await wait(15000);
-
-      const options2 = {
-        method: 'GET',
-        url: `https://octet-api.blockchainapi.io/2.0/wallets/740/transactions/sign/${response.data.uuid}`,
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer ' + octet,
-        }
-      };
-
-      const response2 = await axios.request(options2)
-      console.log("res2 : ", response2.data.serializedSignedTransaction)
-
-      return formatJsonRpcResult(id, response2.data.serializedSignedTransaction)
-
-
-      //const signTransaction = request.params[0]
-      //const signature = await wallet.signTransaction(signTransaction)
-      //return formatJsonRpcResult(id, signature)
+      const signTransaction = request.params[0]
+      const signature = await wallet.signTransaction(signTransaction)
+      return formatJsonRpcResult(id, signature)
 
     default:
       throw new Error(getSdkError('INVALID_METHOD').message)
